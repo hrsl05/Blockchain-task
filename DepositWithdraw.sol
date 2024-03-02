a6dc61721ce23eff09c6b848edd4c345fae5186f
Harshul Singal <160569665+hrsl05@users.noreply.github.com>
2024-03-02 21:40:47 +0530

    Update DepositWithdraw.sol

--- DepositWithdraw.sol
+++ DepositWithdraw.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.8;
 
 contract Deposit{
-    uint256 public MinETH = 100 gwei;
+    uint256 public MinETH = 0 gwei;
     address[] public FromAddressList;
     mapping(address => uint256) public FromAddrAmtMap;
     function depositETH() public payable{
@@ -15,4 +15,16 @@ contract Deposit{
         (bool callSuccess, ) = payable(msg.sender).call{value: _CryptoAmount}("");
         require(callSuccess, "My Call failed");
     }
+    
+    function getBalance(address contractAddress) public view returns(uint){
+        return contractAddress.balance;
+    }
+
+   
+    function currentBalance() public view returns (uint) {
+        return address(this).balance; 
+    }
+
+    receive() external payable {}
+
 }
